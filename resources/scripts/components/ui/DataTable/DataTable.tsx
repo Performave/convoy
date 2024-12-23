6e8d1365a35346e9aba769606e133b8bc7e62492
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-12-22 18:57:06 -0600

    Add datatables

--- resources/scripts/components/ui/DataTable/DataTable.tsx
+++ resources/scripts/components/ui/DataTable/DataTable.tsx
@@ -1,8 +1,11 @@
-import { cn } from '@/utils'
-import { type Table as TanstackTable, flexRender } from '@tanstack/react-table'
-import * as React from 'react'
+'use client'
 
-import { getCommonPinningStyles } from '@/lib/data-table'
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from '@tanstack/react-table'
 
 import {
     Table,
@@ -13,110 +16,72 @@ import {
     TableRow,
 } from '@/components/ui/Table'
 
-import DataTablePagination from './DataTablePagination.tsx'
-
-interface DataTableProps<TData> extends React.HTMLAttributes<HTMLDivElement> {
-    /**
-     * The table instance returned from useDataTable hook with pagination, sorting, filtering, etc.
-     * @type TanstackTable<TData>
-     */
-    table: TanstackTable<TData>
-
-    /**
-     * The floating bar to render at the bottom of the table on row selection.
-     * @default null
-     * @type React.ReactNode | null
-     * @example floatingBar={<TasksTableFloatingBar table={table} />}
-     */
-    floatingBar?: React.ReactNode | null
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
 }
 
-const DataTable = <TData,>({
-    table,
-    floatingBar = null,
-    children,
-    className,
-    ...props
-}: DataTableProps<TData>) => {
+const DataTable = <TData, TValue>({
+    columns,
+    data,
+}: DataTableProps<TData, TValue>) => {
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
     return (
-        <div
-            className={cn('w-full space-y-2.5 overflow-auto', className)}
-            {...props}
-        >
-            {children}
-            <div className='overflow-hidden rounded-md border'>
-                <Table>
-                    <TableHeader>
-                        {table.getHeaderGroups().map(headerGroup => (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map(header => {
-                                    return (
-                                        <TableHead
-                                            key={header.id}
-                                            colSpan={header.colSpan}
-                                            style={{
-                                                ...getCommonPinningStyles({
-                                                    column: header.column,
-                                                }),
-                                            }}
-                                        >
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                      header.column.columnDef
-                                                          .header,
-                                                      header.getContext()
-                                                  )}
-                                        </TableHead>
-                                    )
-                                })}
+        <div className='rounded-md border'>
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map(header => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                  header.column.columnDef
+                                                      .header,
+                                                  header.getContext()
+                                              )}
+                                    </TableHead>
+                                )
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map(row => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && 'selected'}
+                            >
+                                {row.getVisibleCells().map(cell => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext()
+                                        )}
+                                    </TableCell>
+                                ))}
                             </TableRow>
-                        ))}
-                    </TableHeader>
-                    <TableBody>
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map(row => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={
-                                        row.getIsSelected() && 'selected'
-                                    }
-                                >
-                                    {row.getVisibleCells().map(cell => (
-                                        <TableCell
-                                            key={cell.id}
-                                            style={{
-                                                ...getCommonPinningStyles({
-                                                    column: cell.column,
-                                                }),
-                                            }}
-                                        >
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
-                                            )}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell
-                                    colSpan={table.getAllColumns().length}
-                                    className='h-24 text-center'
-                                >
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </div>
-            <div className='flex flex-col gap-2.5'>
-                <DataTablePagination table={table} />
-                {table.getFilteredSelectedRowModel().rows.length > 0 &&
-                    floatingBar}
-            </div>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell
+                                colSpan={columns.length}
+                                className='h-24 text-center'
+                            >
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
         </div>
     )
 }
---
+++ resources/scripts/components/ui/DataTable/index.ts
@@ -0,0 +1,5 @@
+import DataTable from './DataTable'
+import DataTablePagination from './DataTablePagination'
+import DataTableViewOptions from './DataTableViewOptions'
+
+export { DataTable, DataTablePagination, DataTableViewOptions }
